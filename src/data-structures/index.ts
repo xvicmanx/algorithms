34b57fbe447d2e0dd1352d9c75a370e3a4add9ac
Victor Trejo <vic3jo@gmail.com>
2020-11-02 09:20:48 -0400

    Implement set data structure

--- src/data-structures/index.ts
+++ src/data-structures/index.ts
@@ -3,3 +3,5 @@
 export { default as Stack } from './stack';
 export { default as Queue } from './queue';
 export { default as Dequeue } from './dequeue';
+export { default as LinkedList } from './linked-list';
+export { default as ItemsSet } from './items-set';
