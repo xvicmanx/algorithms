78d2d82269e7cca6135bbe9166c4e979cd1b2962
Victor Trejo <vic3jo@gmail.com>
2020-11-11 13:59:45 -0400

    Add graph data structure

---
+++ src/data-structures/graph.test.ts
@@ -0,0 +1,138 @@
+import { expect } from 'chai';
+import Graph from './graph';
+
+
+describe('Graph', () => {
+  let ds;
+  let vertices;
+  let edges;
+
+  const initGraph = (directed = false) => {
+    ds = new Graph(directed);
+    vertices = ['A', 'B', 'C', 'D', 'E'];
+    edges = [
+      ['A', 'B'],
+      ['A', 'C'],
+      ['B', 'D'],
+      ['C', 'E'],
+      ['C', 'D'],
+      ['E', 'F']
+    ];
+
+    vertices.forEach((vertice) => {
+      ds.addVertice(vertice);
+    });
+
+    edges.forEach((edge) => {
+      ds.addEdge(edge[0], edge[1]);
+    });
+  };
+
+  context('Undirected', () => {
+    beforeEach(() => {
+      initGraph(false);
+    });
+  
+    describe('vertices', () => {
+      it('does return all the vertices', () => {
+        expect(ds.vertices()).eql([...vertices, 'F']);
+      });
+    });
+  
+    describe('adjacents', () => {
+      it('does return adjacents for each vertice', () => {
+        expect(ds.adjacents('A')).eql(['B', 'C']);
+        expect(ds.adjacents('B')).eql(['A', 'D']);
+        expect(ds.adjacents('C')).eql(['A', 'E', 'D']);
+        expect(ds.adjacents('D')).eql(['B', 'C']);
+        expect(ds.adjacents('E')).eql(['C', 'F']);
+        expect(ds.adjacents('F')).eql(['E']);
+      });
+  
+      it('does return an empty collection for a newly added vertice', () => {
+        ds.addVertice('G');
+        expect(ds.adjacents('G')).eql([]);
+      });
+    });
+  
+    describe('clear', () => {
+      it('removes all the elements from the collection', () => {
+        ds.clear();
+        expect(ds.vertices()).eql([]);
+      });
+    });
+  
+    describe('toString', () => {
+      it('returns an empty string by default when there are no elements', () => {
+        ds.clear();
+        expect(ds.toString()).equal('');
+      });
+    
+      it('returns a string representation of the structure', () => {
+        const expectedString = [
+          'A => B, C',
+          'B => A, D',
+          'C => A, E, D',
+          'D => B, C',
+          'E => C, F',
+          'F => E',
+        ].join('\n');
+        expect(ds.toString()).equal(expectedString);
+      });
+    });
+  });
+
+  context('Directed', () => {
+    beforeEach(() => {
+      initGraph(true);
+    });
+  
+    describe('vertices', () => {
+      it('does return all the vertices', () => {
+        expect(ds.vertices()).eql([...vertices, 'F']);
+      });
+    });
+  
+    describe('adjacents', () => {
+      it('does return adjacents for each vertice', () => {
+        expect(ds.adjacents('A')).eql(['B', 'C']);
+        expect(ds.adjacents('B')).eql(['D']);
+        expect(ds.adjacents('C')).eql(['E', 'D']);
+        expect(ds.adjacents('D')).eql([]);
+        expect(ds.adjacents('E')).eql(['F']);
+        expect(ds.adjacents('F')).eql([]);
+      });
+  
+      it('does return an empty collection for a newly added vertice', () => {
+        ds.addVertice('G');
+        expect(ds.adjacents('G')).eql([]);
+      });
+    });
+  
+    describe('clear', () => {
+      it('removes all the elements from the collection', () => {
+        ds.clear();
+        expect(ds.vertices()).eql([]);
+      });
+    });
+  
+    describe('toString', () => {
+      it('returns an empty string by default when there are no elements', () => {
+        ds.clear();
+        expect(ds.toString()).equal('');
+      });
+    
+      it('returns a string representation of the structure', () => {
+        const expectedString = [
+          'A => B, C',
+          'B => D',
+          'C => E, D',
+          'D => ',
+          'E => F',
+          'F => ',
+        ].join('\n');
+        expect(ds.toString()).equal(expectedString);
+      });
+    });
+  });
+});
---
+++ src/data-structures/graph.ts
@@ -0,0 +1,117 @@
+// @ts-check
+
+const DEFAULT_KEY_GENERATOR = (value: any) => `${value}`;
+
+/**
+ * A Graph
+ * */
+class Graph {
+  // To store the graph's vertices
+  private _vertices: Array<any>;
+  // To store vertices adjacents
+  private _adjacents: { [key: string]: Array<any> };
+  // A generator of a key for the vertice
+  private _keyGenerator: Function;
+  // To indicate if the graph is directed or not
+  private _directed: boolean;
+
+  /**
+   * Graph Constructor
+   */
+  constructor(
+    directed: boolean = false,
+    keyGenerator: Function = DEFAULT_KEY_GENERATOR,
+  ) {
+    this._vertices = [];
+    this._adjacents = {};
+    this._directed = directed;
+    this._keyGenerator = keyGenerator
+  }
+
+  /**
+   * Adds a vertice to the graph
+   * @param {any} vertice - The item to be inserted
+   */
+  addVertice(vertice: any) {
+    if (!this._vertices.includes(vertice)) {
+      this._vertices.push(vertice);
+    }
+  }
+
+  /**
+   * Adds an edge between vertices
+   * @param {any} vertice first vertice
+   * @param {any} anotherVertice second vertice
+   */
+  addEdge(vertice: any, anotherVertice: any) {
+    if (
+      !this._validVertice(vertice) ||
+      !this._validVertice(anotherVertice)
+    ) {
+      return;
+    }
+
+    this.addVertice(vertice);
+    this.addVertice(anotherVertice);
+    this._addAdjacent(vertice, anotherVertice);
+
+    if (!this._directed) {
+      this._addAdjacent(anotherVertice, vertice);
+    }
+  }
+
+  /**
+   * Returns graph vertices
+   * @return {Array} The vetices
+   */
+  vertices(): Array<any> {
+    return [...this._vertices];
+  }
+
+  /**
+   * Returns graph vertices
+   * @return {Array} The vetices
+   */
+  adjacents(vertice: any): Array<any> {
+    if (!this._validVertice(vertice)) {
+      return [];
+    }
+
+    const verticeKey = this._keyGenerator(vertice);
+    this._adjacents[verticeKey] = this._adjacents[verticeKey] || [];
+    return [...this._adjacents[verticeKey]];
+  }
+
+  /**
+   * Clears the graph
+   */
+  clear() {
+    this._vertices = [];
+    this._adjacents = {};
+  }
+
+  /**
+   * Returns the string representation of the graph
+   * @return {string} the string representation of the graph.
+   */
+  toString(): string {
+    return this._vertices.map((vertice) => {
+      const adjacents = this.adjacents(vertice);
+      return `${vertice} => ${adjacents.join(', ')}`;
+    }).join('\n');
+  }
+
+  private _validVertice(vertice: any): boolean {
+    return vertice !== null && vertice !== undefined;
+  }
+
+  private _addAdjacent(vertice: string, adjacentVertice: any) {
+    const verticeKey = this._keyGenerator(vertice);
+    this._adjacents[verticeKey] = this._adjacents[verticeKey] || [];
+    if (!this._adjacents[verticeKey].includes(adjacentVertice)) {
+      this._adjacents[verticeKey].push(adjacentVertice);
+    }
+  }
+}
+
+export default Graph;
